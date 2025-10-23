d537668d7680bd543ef3ba81c1c728404340eb89
Alireza-Hamidi2001 <alireza.hamidi.eng@gmail.com>
2025-10-23 23:04:14 +0330

    add CORS

--- api/menu.js
+++ api/menu.js
@@ -120,7 +120,13 @@ const menu = {
         },
     ],
 };
-
 export default function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); 
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
     res.status(200).json(menu);
 }
